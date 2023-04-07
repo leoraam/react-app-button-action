e8897d1f7297fef713b6a6b2d5e6c3183bc28066
Leonardo <leoraam@mac.com>
2023-04-07 21:08:30 +0200

    Passing Children

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = ["New York", "Madrid", "London", "Buenos Aires", "Paris"];
-
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      <Alert>Hello World</Alert>
     </div>
   );
 }
