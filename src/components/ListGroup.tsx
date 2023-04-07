2b8d8da92584d67d3bcf52e0c7a0a6cfc3fefb0a
Leonardo <leoraam@mac.com>
2023-04-07 17:02:21 +0200

    no items message function

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,9 +1,11 @@
 function ListGroup() {
-  const items = ["New York", "Madrid", "London", "Buenos Aires", "Paris"];
+  let items = ["New York", "Madrid", "London", "Buenos Aires", "Paris"];
+  //items = [];
 
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
