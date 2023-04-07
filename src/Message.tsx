73737e65457351d00e913b5840107bf8fab20a0b
Leonardo <leoraam@mac.com>
2023-04-07 14:48:28 +0200

    Hello plus constant

--- src/Message.tsx
+++ src/Message.tsx
@@ -2,6 +2,9 @@
 // PascalCasing or CameCase
 function Message () {
   // JSX: JavaScript XML
+  const name = 'Leonardo';
+  if (name)
+    return <h1>Hello {name}!</h1>;
   return <h1>Hello World</h1>;
 }
 
