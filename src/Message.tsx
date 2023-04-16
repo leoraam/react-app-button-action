8dbc1d430185c27cfdfb1e1bdaa32413c8ca1404
Leonardo <leoraam@mac.com>
2023-04-16 14:32:38 +0200

    update

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,10 +1,8 @@
-
 // PascalCasing or CameCase
-function Message () {
+function Message() {
   // JSX: JavaScript XML
-  const name = 'Leonardo';
-  if (name)
-    return <h1>Hello {name}!</h1>;
+  const name = "Leonardo";
+  if (name) return <h1>Hello {name}!</h1>;
   return <h1>Hello World</h1>;
 }
 
